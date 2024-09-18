00a43a322fc2c1a6ff15725322692676af4d55d0
Amelia Sha <165308233+Accelerator-CC@users.noreply.github.com>
2024-09-18 20:09:33 +0000

    change adj nouns and verbs

---
+++ js/nouns/accelerator.js
@@ -0,0 +1,12 @@
+nouns = nouns.concat([
+    'bunny',
+    'Tony Stark',
+    'Captain America',
+    'Thanos',
+    'Spiderman',
+    'Elon Musk',
+    'Hello Kitty',
+    'Professor Snape',
+    'Superman',
+    'Harry Poter'
+]);
--- js/verbs/kyle.js
+++ js/verbs/kyle.js
@@ -1,5 +1,7 @@
 verbs = verbs.concat([
     'run',
     'jump',
-    'code'
+    'code',
+    'punch',
+    'sing'
 ]);
